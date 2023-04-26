9dfa660d18ac73ad5bba711fed5412f3f7e078a1
enzo kao <108316552+enzoShao@users.noreply.github.com>
2023-04-26 22:25:54 +0800

    refactor: 💡 移除多餘console.log；調整拼字錯誤

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -3,13 +3,13 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('mongo', () => {
   const username = process.env.MONGO_USERNAME;
   const password = encodeURIComponent(process.env.MONGO_PASSWORD);
-  const resourse = process.env.MONGO_RESOURSE;
-  const uri = `mongodb+srv://${username}:${password}@${resourse}`;
+  const resource = process.env.MONGO_RESOURCE;
+  const uri = `mongodb+srv://${username}:${password}@${resource}`;
 
   return {
     username,
     password,
-    resourse,
+    resource,
     uri,
   };
 });
