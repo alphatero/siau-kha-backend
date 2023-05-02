328fcc5e7cfca49071d715baeafbb9745ae57c83
amy <amy>
2023-05-02 15:47:42 +0800

    refactor: 💡 remove unused code

--- src/core/models/summary/summary.model.ts
+++ src/core/models/summary/summary.model.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { ProductTags } from '../product-tags';
 import { User } from '../user';
 
 export type SummaryDocument = Summary & Document;
