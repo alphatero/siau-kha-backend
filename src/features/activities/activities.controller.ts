0a7e0eb384c09fd670ba9c83a96f4ae4a2d1a870
enzo kao <108316552+enzoShao@users.noreply.github.com>
2023-05-16 14:11:53 +0800

    refactor: 💡  新增 activities 相關 apiExample

--- src/features/activities/activities.controller.ts
+++ src/features/activities/activities.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/swagger';
 import { JwtGuard } from 'src/common/guards';
 import { ActivitiesService } from './activities.service';
+import { getActivityExample } from './apiExample';
 import { CreateActivityDto } from './dto/create-activity.dto';
 @ApiTags('activities')
 @UseGuards(JwtGuard)
@@ -20,24 +21,7 @@ export class ActivitiesController {
   @ApiResponse({
     status: 200,
     schema: {
-      example: {
-        status: 'success',
-        message: '成功',
-        activities: {
-          table_list: [
-            {
-              id: '6459e2c4f30c49fdcfeff467',
-              activities_name: '打卡優惠活動',
-              discount_type: '1',
-              min_spend: 99,
-              is_period: true,
-              start_time: '2023-05-02T07:23:22.000Z',
-              end_time: '2023-06-30T11:22:22.000Z',
-              act_products_list: ['644e9a33020c9409fe694dc4'],
-            },
-          ],
-        },
-      },
+      example: getActivityExample,
     },
   })
   @Get('/list')
---
+++ src/features/activities/apiExample/index.ts
@@ -0,0 +1,18 @@
+export const getActivityExample = {
+  status: 'success',
+  message: '成功',
+  activities: {
+    table_list: [
+      {
+        id: '6459e2c4f30c49fdcfeff467',
+        activities_name: '打卡優惠活動',
+        discount_type: '1',
+        min_spend: 99,
+        is_period: true,
+        start_time: '2023-05-02T07:23:22.000Z',
+        end_time: '2023-06-30T11:22:22.000Z',
+        act_products_list: ['644e9a33020c9409fe694dc4'],
+      },
+    ],
+  },
+};
