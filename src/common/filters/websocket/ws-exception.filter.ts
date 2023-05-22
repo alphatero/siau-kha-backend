fcf949c248ec9476097a2dabacc4e05f7b71ebd0
eddie <eddie605134@gmailcom>
2023-05-23 01:03:54 +0800

    refactor: 💡 ws exception 改寫

--- src/common/filters/websocket/ws-exception.filter.ts
+++ src/common/filters/websocket/ws-exception.filter.ts
@@ -1,20 +1,20 @@
-import { Catch, ArgumentsHost, WsExceptionFilter } from '@nestjs/common';
-import { WsException } from '@nestjs/websockets';
+import { Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { WsException, BaseWsExceptionFilter } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@Catch(WsException)
-export class WebSocketExceptionFilter implements WsExceptionFilter {
-  catch(exception: WsException, host: ArgumentsHost): any {
+@Catch(WsException, HttpException)
+export class WebSocketExceptionFilter extends BaseWsExceptionFilter {
+  catch(exception: WsException | HttpException, host: ArgumentsHost): any {
     const ctx = host.switchToWs();
     const client = ctx.getClient<Socket>();
+    const error =
+      exception instanceof WsException
+        ? exception.getError()
+        : exception.getResponse();
+    const details = error instanceof Object ? { ...error } : { message: error };
 
-    // 取得錯誤訊息
-    const wsError = exception.getError();
-
-    const message = 'socket error';
-
-    console.log('wsError', wsError);
+    console.log('details', details);
     // 發送錯誤訊息給客戶端
-    client.emit('onError', { status: 'error', message });
+    client.emit('onError', { status: 'error', message: details.message });
   }
 }
