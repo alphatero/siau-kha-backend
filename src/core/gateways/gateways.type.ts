6a82c327fe7e11c292672d62e78e307a405d5fc3
amy <amy>
2023-05-27 14:31:50 +0800

    feat: 🎸 增加 SUBSCRIBE

--- src/core/gateways/gateways.type.ts
+++ src/core/gateways/gateways.type.ts
@@ -4,5 +4,6 @@ export enum GATEWAY_NAMESPACE {
 
 export enum SUBSCRIBE {
   ORDER_PRODUCT_DETAILS = 'order-product-details', // 送出訂單
-  PRODUCT_DETAILS = 'product-details', // 出菜、上菜、退點
+  UPDATE_PRODUCT_DETAILS = 'update-product-details', // 出菜、上菜
+  DELETE_PRODUCT_DETAILS = 'delete-product-details', // 退點
 }
