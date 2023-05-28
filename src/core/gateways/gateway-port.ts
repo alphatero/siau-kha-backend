5cd545a8e8b802c192c468c50d5ee7372c964f7e
amy <amy>
2023-05-28 12:43:18 +0800

    fix: 🐛 gateways type  統一導出 & 移除 port 設定

--- src/core/gateways/gateway-port.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-import { GATEWAY_NAMESPACE } from './gateways.type';
-
-dotenv.config();
-
-// key: namespace, value: port
-export const gatewayPort = {
-  [GATEWAY_NAMESPACE.ORDER]: parseInt(process.env.ORDER_PRODUCT_DETAILS_PORT),
-};
