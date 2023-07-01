dd3c8d27698b99324483857452d99fef5ea134ee
yuyu0905 <86718154+yuyu0905@users.noreply.github.com>
2023-07-01 23:25:40 +0800

    feat: 🎸 2023-07-01T23:25

--- dist/features/check-out/check-out.service.js
+++ dist/features/check-out/check-out.service.js
@@ -95,7 +95,7 @@ let CheckOutService = class CheckOutService {
             finalTotal = validTotal;
         }
         const activity_charge = finalTotal - validTotal;
-        const service_charge = finalTotal * 0.1;
+        const service_charge = Math.round(finalTotal * 0.1);
         finalTotal = finalTotal + service_charge;
         await this.orderModel
             .findByIdAndUpdate(id, { final_total: finalTotal }, { new: true })
